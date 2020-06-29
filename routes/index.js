607e09a0b7350c3cb07748ec4f4547e2d20eb33d
Jordan M <jordan.muliaman@gmail.com>
2020-06-28 18:42:12 -0700

    rename routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
 router.use('/api', require('./bookRoutes.js'))
-router.use('/api', require('./searchRoutes.js'))
+router.use('/api', require('./googleRoutes.js'))
 
 module.exports = router
